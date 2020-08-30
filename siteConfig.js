82da229477bdfafbe57d4de814942618e5ac44db
Quentin Brogly <qbrogly@gmail.com>
2020-08-29 17:06:03 -0700

    Initial commit

---
+++ siteConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "LFlores Art",
+  shortName: "LFlores Art",
+  description: "Hi! Welcome to LFlores Art",
+  url: "https://gatsby-london.netlify.com", // Domain of your site without prefix!
+  prefix: "/",
+  author: "Quentin Brogly",
+  twitter: "",
+}
